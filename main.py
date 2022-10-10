6bb70244a8e4369eaf023ee25068a6ecaab861c2
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-10 16:54:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,20 @@ num2 = list(map(int, input().split()))
 # ******************************
 # Make your Code
 # ******************************
+num3 = []
 
-# print (num3) 
+
+index = 0
+while True:
+    if index <= len(num1) - 1:
+        num3.append(num1[index])
+        num3.append(num2[index])
+    elif index > len(num1) - 1 and index <= len(num2) - 1:
+        num3.append(num2[index])
+    else:
+        break
+
+    # incrementing the index
+    index += 1
+
+print(num3)
